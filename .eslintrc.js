c99cc3805433af3db31844832360acac4352ee29
yukina1418 <yukina1418@gmail.com>
2022-09-13 22:50:17 +0900

    프리티어, 린트 설정 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,17 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'typeLike',
+        format: ['PascalCase'],
+      },
+      {
+        selector: 'variable',
+        format: ['camelCase', 'UPPER_CASE'],
+        leadingUnderscore: 'allow',
+      },
+    ],
   },
 };
